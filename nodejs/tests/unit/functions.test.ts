b2311726c6951ec56462d569a7aa2fb7e94b3a3d
Yasir Farooqui <yasir@integry.io>
2024-12-17 10:52:20 +0500

    added functions tests.

--- nodejs/tests/unit/functions.test.ts
+++ nodejs/tests/unit/functions.test.ts
@@ -1,9 +1,8 @@
 import integry from "@integry/server-sdk";
 
-const IntegrySDK = require("../../src/integry");
-const appKey = "3883c794-9d71-450e-b39d-dfa21ffab006";
-const appSecret = "ffd3a8d4-7160-4a44-b603-08848420a108";
-const userId = "yasir@integry.io";
+const appKey = "<YOUR-INTEGRY-APP-KEY>";
+const appSecret = "YOUR-INTEGRY-APP-SECRET";
+const userId = "<YOUR-USER-ID>";
 
 describe("IntegrySDK", () => {
   const sdk = new integry({
@@ -23,6 +22,57 @@ describe("IntegrySDK", () => {
     expect(functions.functions[0]).toHaveProperty("name");
     expect(functions.functions[0]).toHaveProperty("description");
     expect(functions.functions[0]).toHaveProperty("parameters");
+  });
+
+  it("should fetch a specific function", async () => {
+    const functionName = "slack-post-message";
+    const functionSpec = await sdk.functions.get(functionName, {
+      userId: userId,
+    });
+    expect(functionSpec).toBeDefined();
+    expect(functionSpec).toBeInstanceOf(Object);
+    expect(functionSpec).toHaveProperty("name");
+    expect(functionSpec).toHaveProperty("description");
+    expect(functionSpec).toHaveProperty("parameters");
+  });
+
+  it("should predict a function based on the passed prompt", async () => {
+    const prompt = "Post a message to slack";
+    const prediction = await sdk.functions.predict({
+      userId: userId,
+      prompt: prompt,
+    });
+    expect(prediction).toBeDefined();
+    expect(prediction).toBeInstanceOf(Object);
+    expect(prediction).toHaveProperty("functions");
+    expect(prediction.functions).toBeInstanceOf(Array);
+    expect(prediction.functions.length).toBeGreaterThan(0);
+    expect(prediction.functions[0]).toHaveProperty("name");
+    expect(prediction.functions[0]).toHaveProperty("description");
+    expect(prediction.functions[0]).toHaveProperty("parameters");
+  }, 10000);
+
+  it("should call a function", async () => {
+    const functionName = "slack-post-message";
+    const connected_account_id = "<YOUR-SLACK-ACCOUNT-ID>";
+    const params = {
+      channel: "{channel}",
+      text: "Hello, world from {name}!",
+    };
+    const variables = {
+      channel: "test_slack",
+      name: "Integry",
+    };
+    const response = await sdk.functions.call(functionName, {
+      userId: userId,
+      connected_account_id: connected_account_id,
+      params: params,
+      variables: variables,
+    });
+    expect(response).toBeDefined();
+    expect(response).toBeInstanceOf(Object);
+    expect(response).toHaveProperty("network_code");
+    expect(response.network_code).toBe("200");
   });
 
   // Add more tests
